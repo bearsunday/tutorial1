13eb56be79d59d9cad5cbafc7663dc53a13422b9
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 08:45:15 +0900

    add /weekday app resource

---
+++ src/Resource/App/Weekday.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+
+class Weekday extends ResourceObject
+{
+    public function onGet($year, $month, $day)
+    {
+        $date = \DateTime::createFromFormat('Y-m-d', "$year-$month-$day");
+        $this['weekday'] = $date->format("D");
+
+        return $this;
+    }
+}
