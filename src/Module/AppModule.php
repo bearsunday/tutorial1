d8b58d1c5920be8b6c04c693880bf07df0f51e77
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 08:53:56 +0900

    bind Monolog interface

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -3,8 +3,10 @@
 namespace MyVendor\Weekday\Module;
 
 use BEAR\Package\PackageModule;
+use Psr\Log\LoggerInterface;
 use Ray\Di\AbstractModule;
-use BEAR\Package\Provide\Router\AuraRouterModule; // この行を追加
+use BEAR\Package\Provide\Router\AuraRouterModule;
+use Ray\Di\Scope; // この行を追加
 
 class AppModule extends AbstractModule
 {
@@ -15,5 +17,7 @@ class AppModule extends AbstractModule
     {
         $this->install(new PackageModule);
         $this->override(new AuraRouterModule); // この行を追加
+
+        $this->bind(LoggerInterface::class)->toProvider(MonologLoggerProvider::class)->in(Scope::SINGLETON);
     }
 }
---
+++ src/Module/MonologLoggerProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MyVendor\Weekday\Module;
+
+use BEAR\AppMeta\AbstractAppMeta;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Ray\Di\ProviderInterface;
+
+class MonologLoggerProvider implements ProviderInterface
+{
+    /**
+     * @var AbstractAppMeta
+     */
+    private $appMeta;
+
+    public function __construct(AbstractAppMeta $appMeta)
+    {
+        $this->appMeta = $appMeta;
+    }
+
+    public function get()
+    {
+        $log = new Logger('weekday');
+        $log->pushHandler(
+            new StreamHandler($this->appMeta->logDir . '/weekday.log')
+        );
+
+        return $log;
+    }
+}
