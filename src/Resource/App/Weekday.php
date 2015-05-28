e21d0c588e36ddbda26f0b34b7939b2183ecd611
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 09:02:59 +0900

    use logger

--- src/Resource/App/Weekday.php
+++ src/Resource/App/Weekday.php
@@ -3,13 +3,22 @@
 namespace MyVendor\Weekday\Resource\App;
 
 use BEAR\Resource\ResourceObject;
+use Psr\Log\LoggerInterface;
 
 class Weekday extends ResourceObject
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function onGet($year, $month, $day)
     {
         $date = \DateTime::createFromFormat('Y-m-d', "$year-$month-$day");
         $this['weekday'] = $date->format("D");
+        $this->logger->info("$year-$month-$day {$this['weekday']}");
 
         return $this;
     }
