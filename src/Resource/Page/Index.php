7d2e391e92c0181907479c3d44d7afae2e31da46
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 09:56:52 +0900

    add page resource and embed app resource

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -3,12 +3,18 @@
 namespace MyVendor\Weekday\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
+use BEAR\Resource\Annotation\Embed;
 
 class Index extends ResourceObject
 {
-    public function onGet($name = 'BEAR.Sunday')
+    /**
+     * @Embed(rel="weekday", src="app://self/weekday{?year,month,day}")
+     */
+    public function onGet($year, $month, $day)
     {
-        $this['greeting'] = 'Hello ' . $name;
+        $this['year'] = $year;
+        $this['month'] = $month;
+        $this['day'] = $day;
 
         return $this;
     }
