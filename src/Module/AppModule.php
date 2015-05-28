6904badd3c1cfbfa0f90aa7f269f8a1dcfcf58cb
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 10:02:42 +0900

    install CakeDbModule

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -6,6 +6,7 @@ use BEAR\Package\PackageModule;
 use MyVendor\Weekday\Annotation\BenchMark;
 use MyVendor\Weekday\Interceptor\BenchMarker;
 use Psr\Log\LoggerInterface;
+use Ray\CakeDbModule\CakeDbModule;
 use Ray\Di\AbstractModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
 use Ray\Di\Scope;
@@ -27,5 +28,11 @@ class AppModule extends AbstractModule
             $this->matcher->annotatedWith(BenchMark::class),
             [BenchMarker::class]
         );
+
+        $dbConfig = [
+            'driver' => 'Cake\Database\Driver\Sqlite',
+            'database' => dirname(dirname(__DIR__)) . '/var/db/todo.sqlite3'
+        ];
+        $this->install(new CakeDbModule($dbConfig));
     }
 }
