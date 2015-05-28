df80ae9a9b1a844c427962c8cf5012968434cc87
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 10:03:55 +0900

    add todo resource

---
+++ src/Resource/App/Todo.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+use Ray\CakeDbModule\DatabaseInject;
+
+class Todo extends ResourceObject
+{
+    use DatabaseInject;
+
+    public function onGet($id)
+    {
+        $this['todo'] = $this
+            ->db
+            ->newQuery()
+            ->select('*')
+            ->from('todo')
+            ->where(['id' => $id])
+            ->execute()
+            ->fetchAll('assoc');
+
+        return $this;
+    }
+
+    public function onPost($todo)
+    {
+        $statement = $this->db->insert(
+            'todo',
+            ['todo' => $todo, 'created' => new \DateTime('now')],
+            ['created' => 'datetime']
+        );
+        // hyper link
+        $this->headers['Location'] = '/todo/?id=' . $statement->lastInsertId();
+        // status code
+        $this->code = 201;
+
+        return $this;
+    }
+}
