4fe77d57cec82cdd1eb493ad851a2d3d1f73fec9
Akihito Koriyama <akihito.koriyama@gmail.com>
2015-05-28 14:14:13 +0900

    add one more application

---
+++ my-vendor/Acme.Blog/src/Module/App.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Acme\Blog\Module;
+
+use BEAR\Sunday\Extension\Application\AbstractApp;
+
+class App extends AbstractApp
+{
+}
---
+++ my-vendor/Acme.Blog/src/Module/AppModule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Acme\Blog\Module;
+
+use BEAR\Package\PackageModule;
+use Ray\Di\AbstractModule;
+
+class AppModule extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->install(new PackageModule);
+    }
+}
