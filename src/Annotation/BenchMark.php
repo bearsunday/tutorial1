1873f7df14d5acceb6a424ad90b3811140a3e0ad
koriym <akihito.koriyama@gmail.com>
2021-05-02 12:11:27 +0900

    AOP

---
+++ src/Annotation/BenchMark.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Annotation;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+final class BenchMark
+{
+}
--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -8,6 +8,8 @@ use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
 use BEAR\Package\Provide\Router\AuraRouterModule;
+use MyVendor\Weekday\Annotation\BenchMark;
+use MyVendor\Weekday\Interceptor\BenchMarker;
 use MyVendor\Weekday\MyLogger;
 use MyVendor\Weekday\MyLoggerInterface;
 
@@ -21,6 +23,11 @@ class AppModule extends AbstractAppModule
         $appDir = $this->appMeta->appDir;
         $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
         $this->bind(MyLoggerInterface::class)->to(MyLogger::class);
+        $this->bindInterceptor(
+            $this->matcher->any(),                                         // どのクラスでも
+            $this->matcher->annotatedWith(BenchMark::class), // #[Attribute]と属性の付けられたメソッドに
+            [BenchMarker::class]                                           // BenchMarkerインターセプターを適用
+        );
         $this->install(new PackageModule());
     }
 }
