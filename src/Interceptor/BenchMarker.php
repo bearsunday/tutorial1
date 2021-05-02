a210a10ce52275e00bcce46ef2a2213158c0b3a6
koriym <akihito.koriyama@gmail.com>
2021-05-02 12:11:27 +0900

    AOP

---
+++ src/Interceptor/BenchMarker.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Interceptor;
+
+use MyVendor\Weekday\MyLoggerInterface;
+use Ray\Aop\MethodInterceptor;
+use Ray\Aop\MethodInvocation;
+
+use function microtime;
+use function sprintf;
+
+class BenchMarker implements MethodInterceptor
+{
+    public function __construct(private MyLoggerInterface $logger)
+    {
+    }
+
+    public function invoke(MethodInvocation $invocation): mixed
+    {
+        $start = microtime(true);
+        $result = $invocation->proceed(); // Invoke original method
+        $time = microtime(true) - $start;
+        $message = sprintf('%s: %0.5f(µs)', $invocation->getMethod()->getName(), $time);
+        $this->logger->log($message);
+
+        return $result;
+    }
+}
