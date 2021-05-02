1ec7094ac5bf4a7ee7fcdc03d8417ef8a4958549
koriym <akihito.koriyama@gmail.com>
2021-05-02 11:37:18 +0900

    Add aura router

--- src/Module/AppModule.php
+++ src/Module/AppModule.php
@@ -7,6 +7,7 @@ namespace MyVendor\Weekday\Module;
 use BEAR\Dotenv\Dotenv;
 use BEAR\Package\AbstractAppModule;
 use BEAR\Package\PackageModule;
+use BEAR\Package\Provide\Router\AuraRouterModule;
 
 use function dirname;
 
@@ -15,6 +16,8 @@ class AppModule extends AbstractAppModule
     protected function configure(): void
     {
         (new Dotenv())->load(dirname(__DIR__, 2));
+        $appDir = $this->appMeta->appDir;
+        $this->install(new AuraRouterModule($appDir . '/var/conf/aura.route.php'));
         $this->install(new PackageModule());
     }
 }
---
+++ var/conf/aura.route.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @see http://bearsunday.github.io/manuals/1.0/ja/router.html
+ * @var \Aura\Router\Map $map
+ */
+
+$map->route('/weekday', '/weekday/{year}/{month}/{day}');
