bearsunday/Tutorial

Add /weekday resource

src/Resource/App/Weekday.php
@@ -1,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+use DateTimeImmutable;
+use MyVendor\Weekday\Exception\InvalidDateTime;
+
+class Weekday extends ResourceObject
+{
+    public function onGet(int $year, int $month, int $day): static
+    {
+        $dateTime = DateTimeImmutable::createFromFormat('Y-m-d', "$year-$month-$day");
+        if (! $dateTime instanceof DateTimeImmutable) {
+            throw new InvalidDateTime("$year-$month-$day");
+        }
+
+        $weekday = $dateTime->format('D');
+        $this->body = ['weekday' => $weekday];
+
+        return $this;
+    }
+}

tests/Resource/App/WeekdayTest.php
@@ -1,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceInterface;
+use MyVendor\Weekday\Exception\InvalidDateTime;
+use MyVendor\Weekday\Injector;
+use PHPUnit\Framework\TestCase;
+
+class WeekdayTest extends TestCase
+{
+    private ResourceInterface $resource;
+
+    protected function setUp(): void
+    {
+        $injector = Injector::getInstance('app');
+        $this->resource = $injector->getInstance(ResourceInterface::class);
+    }
+
+    public function testOnGet(): void
+    {
+        $ro = $this->resource->get('app://self/weekday', ['year' => '2001', 'month' => '1', 'day' => '1']);
+        $this->assertSame(200, $ro->code);
+        $this->assertSame('Mon', $ro->body['weekday']);
+    }
+
+    public function tesInvalidDateTime(): void
+    {
+        $this->expectException(InvalidDateTime::class);
+        $this->resource->get('app://self/weekday', ['year' => '-1', 'month' => '1', 'day' => '1']);
+    }
+}

bearsunday/tutorial1

src/Resource/App/Weekday.php
@@ -1,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+use DateTimeImmutable;
+use MyVendor\Weekday\Exception\InvalidDateTimeException;
+
+class Weekday extends ResourceObject
+{
+    public function onGet(int $year, int $month, int $day): static
+    {
+        $dateTime = DateTimeImmutable::createFromFormat('Y-m-d', "$year-$month-$day");
+        if (! $dateTime instanceof DateTimeImmutable) {
+            throw new InvalidDateTimeException("$year-$month-$day");
+        }
+
+        $weekday = $dateTime->format('D');
+        $this->body = ['weekday' => $weekday];
+
+        return $this;
+    }
+}

tests/Resource/App/WeekdayTest.php
@@ -1,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceInterface;
+use MyVendor\Weekday\Exception\InvalidDateTimeException;
+use MyVendor\Weekday\Injector;
+use PHPUnit\Framework\TestCase;
+
+class WeekdayTest extends TestCase
+{
+    private ResourceInterface $resource;
+
+    protected function setUp(): void
+    {
+        $injector = Injector::getInstance('app');
+        $this->resource = $injector->getInstance(ResourceInterface::class);
+    }
+
+    public function testOnGet(): void
+    {
+        $ro = $this->resource->get('app://self/weekday', ['year' => '2001', 'month' => '1', 'day' => '1']);
+        $this->assertSame(200, $ro->code);
+        $this->assertSame('Mon', $ro->body['weekday']);
+    }
+
+    public function tesInvalidDateTime(): void
+    {
+        $this->expectException(InvalidDateTimeException::class);
+        $this->resource->get('app://self/weekday', ['year' => '-1', 'month' => '1', 'day' => '1']);
+    }
+}
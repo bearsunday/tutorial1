e9bb9c6b1b3d355403b9e8a98ff2e5275325a6d8
koriym <akihito.koriyama@gmail.com>
2021-05-02 11:17:09 +0900

    Add /weekday resource

---
+++ src/Resource/App/Weekday.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceObject;
+use DateTimeImmutable;
+use MyVendor\Weekday\Exception\InvalidDateTimeException;
+
+class Weekday extends ResourceObject
+{
+    public function onGet(int $year, int $month, int $day): static
+    {
+        $dateTime = DateTimeImmutable::createFromFormat('Y-m-d', "$year-$month-$day");
+        if (! $dateTime instanceof DateTimeImmutable) {
+            throw new InvalidDateTimeException("$year-$month-$day");
+        }
+
+        $weekday = $dateTime->format('D');
+        $this->body = ['weekday' => $weekday];
+
+        return $this;
+    }
+}
