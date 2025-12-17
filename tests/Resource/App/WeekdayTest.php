459267c90f82a41344afaa673807010c2859751b
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-12-17 19:12:47 +0900

    Add test for Weekday resource
    - Create WeekdayTest with testOnGet method
    - Verify resource returns 200 status code
    - Verify correct weekday is returned for 2001-01-01 (Monday)

---
+++ tests/Resource/App/WeekdayTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Resource\App;
+
+use BEAR\Resource\ResourceInterface;
+use MyVendor\Weekday\Injector;
+use PHPUnit\Framework\TestCase;
+
+class WeekdayTest extends TestCase
+{
+    private ResourceInterface $resource;
+
+    protected function setUp(): void
+    {
+        $injector = Injector::getInstance('app');
+        $this->resource = $injector->getInstance(ResourceInterface::class);
+    }
+
+    public function testOnGet(): void
+    {
+        $ro = $this->resource->get('app://self/weekday', ['year' => '2001', 'month' => '1', 'day' => '1']);
+        $this->assertSame(200, $ro->code);
+        $this->assertSame('Mon', $ro->body['weekday']);
+    }
+}
