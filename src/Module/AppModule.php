47a8fa1afb9bad0ddac48301f4f6d17019ec521e
Akihito Koriyama <akihito.koriyama@gmail.com>
2025-12-17 19:11:59 +0900

    Initial commit: Create BEAR.Sunday project
    - Vendor: MyVendor
    - Package: Weekday
    - Generated with composer create-project bear/skeleton

---
+++ src/Module/AppModule.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\Weekday\Module;
+
+use Koriym\EnvJson\EnvJson;
+use BEAR\Package\AbstractAppModule;
+use BEAR\Package\PackageModule;
+
+use function dirname;
+
+final class AppModule extends AbstractAppModule
+{
+    protected function configure(): void
+    {
+        (new EnvJson())->load(dirname(__DIR__, 2));
+        $this->install(new PackageModule());
+    }
+}
